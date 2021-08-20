7cc4da1b8221f7ea3d607743fa2587c4b1647640
nprather <nathan.prather@gmail.com>
2021-08-20 14:40:29 -0400

    asdf

---
+++ S14/native-02-camera/src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Plugins,
+  Capacitor,
+  CameraSource,
+  CameraResultType
+} from '@capacitor/core';
+
+@Component({
+  selector: 'app-image-picker',
+  templateUrl: './image-picker.component.html',
+  styleUrls: ['./image-picker.component.scss']
+})
+export class ImagePickerComponent implements OnInit {
+  @Output() imagePick = new EventEmitter<string>();
+  selectedImage: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera')) {
+      return;
+    }
+    Plugins.Camera.getPhoto({
+      quality: 50,
+      source: CameraSource.Prompt,
+      correctOrientation: true,
+      height: 320,
+      width: 200,
+      resultType: CameraResultType.Base64
+    })
+      .then(image => {
+        this.selectedImage = image.base64Data;
+        this.imagePick.emit(image.base64Data);
+      })
+      .catch(error => {
+        console.log(error);
+        return false;
+      });
+  }
+}
