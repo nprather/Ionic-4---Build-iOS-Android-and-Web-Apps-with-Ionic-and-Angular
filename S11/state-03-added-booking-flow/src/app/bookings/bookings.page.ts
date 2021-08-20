652988d6d2ac14a4a5a0eb0b3940704759b91130
nprather <nathan.prather@gmail.com>
2021-08-20 14:39:03 -0400

    asdf

---
+++ S11/state-03-added-booking-flow/src/app/bookings/bookings.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+
+import { BookingService } from './booking.service';
+import { Booking } from './booking.model';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.page.html',
+  styleUrls: ['./bookings.page.scss']
+})
+export class BookingsPage implements OnInit, OnDestroy {
+  loadedBookings: Booking[];
+  private bookingSub: Subscription;
+
+  constructor(private bookingService: BookingService) {}
+
+  ngOnInit() {
+    this.bookingService.bookings.subscribe(bookings => {
+      this.loadedBookings = bookings;
+    });
+  }
+
+  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+    slidingEl.close();
+    // cancel booking wiht id offerId
+  }
+
+  ngOnDestroy() {
+    if (this.bookingSub) {
+      this.bookingSub.unsubscribe();
+    }
+  }
+}
